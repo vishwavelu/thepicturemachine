28bca1b9c60feecb5ccffa7d7a82808b155c1108
vishwa velu <vishwavelu@vishwas-Mac-mini.local>
2020-11-23 13:41:01 +0530

    export

--- assets/js/main.js
+++ assets/js/main.js
@@ -385,12 +385,12 @@
 
 // ...............
 
-var figure = $(".video").hover( hoverVideo, hideVideo );
+// var figure = $(".video").hover( hoverVideo, hideVideo );
 
-function hoverVideo(e) {  
-    $('video', this).get(0).play(); 
-}
+// function hoverVideo(e) {  
+//     $('video', this).get(0).play(); 
+// }
 
-function hideVideo(e) {
-    $('video', this).get(0).pause(); 
-}
+// function hideVideo(e) {
+//     $('video', this).get(0).pause(); 
+// }
